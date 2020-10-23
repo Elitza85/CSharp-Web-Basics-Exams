c27e89b2f0b97f61ee6707ea259aa634c1612c65
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-10-23 13:28:47 +0300

    Start work on SharedTrip exam preparation project

---
+++ SUS/Apps/SharedTrip/Startup.cs
@@ -0,0 +1,21 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedTrip
+{
+    public class Startup : IMvcApplication
+    {
+        public void Configure(List<Route> routeTable)
+        {
+            
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            
+        }
+    }
+}
